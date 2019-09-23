0c52974ccf67ff2e26bad704716b55a0ff811246
sujithmp <sujith.mpEve1991@gmail.com>
2019-09-23 22:51:12 +0530

    basic vue cli usage

---
+++ basic-vue-set-up/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <p>Add here static contents! If you have something which is dynamic then add it in the router view </p>
+    <p> In  the next section we will install bootstrap</p>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /*text-align: center;*/
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
